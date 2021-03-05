2dbc2e29dd43bfaf2379c9b5292cc2f355cdf177
Craig McMahon <cmcmahon@sgl.com>
2021-03-05 14:15:22 -0500

    Empty top module

---
+++ src/hdl/eggtimer.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/05/2021 02:14:18 PM
+// Design Name: 
+// Module Name: eggtimer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module eggtimer(
+
+    );
+endmodule
