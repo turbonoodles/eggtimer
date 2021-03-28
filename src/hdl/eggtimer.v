ffe18d0395e6e241181f9fab3f697972a3ead377
Craig McMahon <cmcmahon@sgl.com>
2021-03-27 22:44:55 -0400

    instatiate two timers in top module

--- src/hdl/eggtimer.v
+++ src/hdl/eggtimer.v
@@ -21,6 +21,34 @@
 
 
 module eggtimer(
-
+    input wire timer_on,
+    input wire reset,
+    input wire clk_100MHz,
+    output wire [6:0] display_cathodes,
+    output wire [7:0] display_anodes,
     );
+
+// generate pulses every 1s for synchronous counters
+wire pulse_1s;
+defparam timer_1s.MAX_COUNT = 5000000;
+defparam timer_1s.CTR_WIDTH = 23; // need 23b to hold 5 000 000
+clock_divider timer_1s(
+    .clk (clk_5MHz),
+    .reset (reset),
+    .pulse (pulse_1s)
+);
+
+// 2ms clock enable signal for 500Hz display refresh
+wire pulse_2ms;
+defparam timer_2ms.MAX_COUNT = 2000;
+defparam timer_2ms.CTR_WIDTH = 11; // 2^11 = 2048
+clock_divider timer_2ms(
+    .clk (clk_5MHz),
+    .reset (reset),
+    .pulse (pulse_2ms)
+);
+
+
+
+
 endmodule
