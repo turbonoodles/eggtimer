797b016b11191fe09a8425f824a99cd3ae639b5b
Craig McMahon <cmcmahon@sgl.com>
2021-04-14 15:53:27 -0400

    Add evening-out code for bargraph

--- src/hdl/bargraph.v
+++ src/hdl/bargraph.v
@@ -28,16 +28,33 @@ module bargraph(
 
 // first we need to know how many seconds each light in the graph represents
 wire [11:0] seconds_per_segment;
+wire [2:0] rem; // "remainder" after integer division by bitshift
+assign rem = seconds_per_segment[2:0]; // every place less than 8 will be a remainder
 assign seconds_per_segment = prog_seconds >> 3; // divide by 8 using a bit shift
 
 // bar graph thresholds
-assign led[7] = ( timer_seconds >= seconds_per_segment * 7 );
-assign led[6] = ( timer_seconds >= seconds_per_segment * 6 );
-assign led[5] = ( timer_seconds >= seconds_per_segment * 5 );
-assign led[4] = ( timer_seconds >= seconds_per_segment * 4 );
-assign led[3] = ( timer_seconds >= seconds_per_segment * 3 );
-assign led[2] = ( timer_seconds >= seconds_per_segment * 2 );
-assign led[1] = ( timer_seconds >= seconds_per_segment );
+// handle remainders by spreading 'evenly' among segments
+// seven - all segments beside zero
+// six - all segments beside zero and seven
+// five - 7, 6, 4, 3, 1
+// four - odd segments only 
+// three - 7, 5, 3
+// two - 7, 4
+// one - 7 only
+assign led[7] = timer_seconds >= ( seconds_per_segment * 7 + 
+    ( rem == 7 )|( rem == 5 )|( rem == 4 )|( rem==3 )|( rem == 2 )|( rem == 1) ); 
+assign led[6] = timer_seconds >= ( seconds_per_segment * 6  + 
+    ( rem == 7 )| (rem == 6 )|( rem == 5 ) );
+assign led[5] = timer_seconds >= ( seconds_per_segment * 5 +
+    ( rem == 7 )|( rem == 6 )|( rem == 4 )|( rem == 3) );
+assign led[4] = timer_seconds >= ( seconds_per_segment * 4 + 
+    ( rem == 7 )|( rem == 6 )|( rem == 5 )|( rem == 2 ) );
+assign led[3] = timer_seconds >= ( seconds_per_segment * 3 + 
+    ( rem == 7 )|( rem == 6 )|( rem == 5 )|( rem == 4 )|( rem == 3) );
+assign led[2] = timer_seconds >= ( seconds_per_segment * 2 +
+    ( rem == 7)|( rem == 6 ) );
+assign led[1] = timer_seconds >= ( seconds_per_segment +
+    ( rem == 7 )|( rem == 6 )|( rem == 5 )|(rem == 4) );
 assign led[0] = ( timer_seconds > 0 );
 
 endmodule
